ea1f051600277aef11d328ffff24ccaed16d28dd
Tuyen Nguyen <tuyennguyen160296@gmail.com>
2021-07-05 18:00:35 +0700

    update export

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-export * from './src/store';
+export { default as authComponentStore } from './src/store';
 export * from './src/index';
-export * from './src/auth-services';
+export { default as authServices } from './src/auth-services';
 export { createAuthorizedApiClient } from './src/authorized-api-client';
 export { createAppTokenApiClient } from './src/app-token-api-client';
 export * from './src/auth-api-client';
