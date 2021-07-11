d0d113dbcaf04b4d5e228400d0622dbafba252a1
Tuyen Nguyen <tuyennguyen160296@gmail.com>
2021-07-11 18:20:48 +0700

    update

--- index.d.ts
+++ index.d.ts
@@ -3,4 +3,9 @@ export * from './src/index';
 export { default as authServices } from './src/services/auth-services';
 export { createAuthorizedApiClient } from './src/api-client/authorized-api-client';
 export { createAppTokenApiClient } from './src/api-client/app-token-api-client';
-export * from './src/login-component';
+export { default as LoginComponent } from './src/login-component';
+export {
+  LoginComponentProps,
+  LoginComponentRef,
+  LoginComponentStyles,
+} from './src/login-component/types';
