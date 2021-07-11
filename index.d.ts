991dd288972938e4b10cc960f7d7d70980748796
Tuyen Nguyen <tuyennguyen160296@gmail.com>
2021-07-11 21:41:33 +0700

    update

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 export { default as authComponentStore } from './src/store';
 export * from './src/index';
-export * from './src/services/auth-services';
+export { default as authServices } from './src/services/auth-services';
 export { createAuthorizedApiClient } from './src/api-client/authorized-api-client';
 export { createAppTokenApiClient } from './src/api-client/app-token-api-client';
 export { default as LoginComponent } from './src/login-component';
