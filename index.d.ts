1b5f422e5b4b39ec0321dc4a899de9612d510aa4
Tuyen Nguyen <tuyennguyen@Tuyen-Nguyens-Macbook.local>
2022-04-28 14:35:03 +0700

    update profile component

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,7 @@ export { AuthServices } from './src/services/auth-services';
 export { createAuthorizedApiClient } from './src/api-client/authorized-api-client';
 export { createAppTokenApiClient } from './src/api-client/app-token-api-client';
 export { default as LoginComponent } from './src/login-component';
+export { default as ProfileComponent } from './src/profile-component';
 export {
   LoginComponentProps,
   LoginComponentRef,
