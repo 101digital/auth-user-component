72b693b473791386b171dc85d1c2bee04bf23968
khoi <khoi@101digital.io>
2023-03-22 11:00:48 +0700

    update version

--- src/adb-login-component/index.tsx
+++ src/adb-login-component/index.tsx
@@ -74,7 +74,7 @@ const ADBLoginComponent: React.FC<ILogin> = (props: ILogin) => {
                       <InputField
                         name="username"
                         returnKeyType="done"
-                        placeholder={'Mobile number'}
+                        placeholder={'Email'}
                         autoCapitalize="none"
                       />
                     </View>
